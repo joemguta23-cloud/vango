145f84063f4715985334273efc0f3f971a6c69a1
joemguta23-cloud <joemguta23@gmail.com>
2026-07-11 19:49:19 +1000

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-const SITE_URL = 'https://getvango.com.au'
+const SITE_URL = 'https://vanute.com.au'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date()
